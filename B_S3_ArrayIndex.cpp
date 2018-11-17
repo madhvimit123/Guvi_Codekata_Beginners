da433143435a2b07fb3d7cd531b8cb21f33174a1
madhvimit123 <40298759+madhvimit123@users.noreply.github.com>
2018-11-17 13:26:34 +0530

    Create B_S3_ArrayIndex.cpp

---
+++ B_S3_ArrayIndex.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,i,a[50];
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>a[i];
+        cout<<a[i]<<" "<<i<<"\n";
+    }
+    return 0;
+}
