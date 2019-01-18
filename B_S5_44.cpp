ab50926010ac56b49522619d06d6c6d80a9e0534
madhvimit123 <40298759+madhvimit123@users.noreply.github.com>
2019-01-19 00:23:08 +0530

    Create B_S5_44.cpp

---
+++ B_S5_44.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<algorithm>
+#include<string.h>
+using namespace std; 
+int main()
+{
+    
+    int n;
+    cin>>n;
+    if(n>=1 && n<=10)
+    cout<<"yes";
+    else cout<<"no";
+
+    return 0;
+}
