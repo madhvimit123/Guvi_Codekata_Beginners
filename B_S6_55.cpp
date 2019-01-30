5d1b1e277e24f0e1a5b08e2ca17b6581823ade36
madhvimit123 <40298759+madhvimit123@users.noreply.github.com>
2019-01-31 00:39:02 +0530

    Create B_S6_55.cpp

---
+++ B_S6_55.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    int n,r,m;
+    cin>>n>>m;
+    if((n*m)%2==0)
+    cout<<"even";
+    else
+    cout<<"odd";
+    return 0;
+}
