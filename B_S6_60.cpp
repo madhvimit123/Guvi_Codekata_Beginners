83d419dd538584bc656041a81385d41545069700
madhvimit123 <40298759+madhvimit123@users.noreply.github.com>
2019-01-31 01:00:34 +0530

    Create B_S6_60.cpp

---
+++ B_S6_60.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int s=0;
+    for(int i=1;i<=n;i++)
+    s+=i;
+    cout<<s;
+    return 0;
+}
