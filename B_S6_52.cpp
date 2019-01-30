8b5f5ea10e90ae0969e5b7fdf01e528f91a57fcd
madhvimit123 <40298759+madhvimit123@users.noreply.github.com>
2019-01-31 01:03:06 +0530

    Create B_S6_52.cpp

---
+++ B_S6_52.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    int n;
+    char *str[10]={"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
+    cin>>n;
+    cout<<str[n-1];
+    return 0;
+}
